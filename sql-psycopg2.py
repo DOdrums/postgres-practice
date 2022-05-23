0904f01ceb9e08d98473444f47f7342308cea98f
DOdrums <dirkornee@hotmail.com>
2022-05-23 11:14:19 +0000

    write queries for python file

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -7,6 +7,27 @@ connection = psycopg2.connect(database="chinook")
 # build a cursor object of the database
 cursor = connection.cursor()
 
+# Query 1 - select all records from the "Artist" table
+cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - select only the "Name" column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only the "Queen" column from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# Query 4 - select only the "Queen" column from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select only the "Queen" column from the "Artist" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 6 - select only the "Queen" column from the "Artist" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# Query 7 - select albums "Aline in Chains"
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [5])
+
 # fetch the results (multiple)
 results = cursor.fetchall()
 
